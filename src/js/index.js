fd2fe1d8c1c95c10207fa3a189e41550e5b5627d
Victor Augusto <victor08022000@gmail.com>
2022-06-29 11:08:53 -0300

    Imagens de introdução

--- src/js/index.js
+++ src/js/index.js
@@ -1,128 +1,130 @@
-// Método da hora
-function atualizarHoras() {
-    var horasAtual = new Date();
-    var horas = String(horasAtual.getHours()).length == 1 ? String("0" + horasAtual.getHours()) : String(horasAtual.getHours());
-    var minutos = String(horasAtual.getMinutes()).length == 1 ? String("0" + horasAtual.getMinutes()) : String(horasAtual.getMinutes());
-    var segundos = String(horasAtual.getSeconds()).length == 1 ? String("0" + horasAtual.getSeconds()) : String(horasAtual.getSeconds());
+function updateHours() {
+    var currentHours = new Date();
+    var hours = String(currentHours.getHours()).length == 1 ? String("0" + currentHours.getHours()) : String(currentHours.getHours());
+    var minutes = String(currentHours.getMinutes()).length == 1 ? String("0" + currentHours.getMinutes()) : String(currentHours.getMinutes());
+    var seconds = String(currentHours.getSeconds()).length == 1 ? String("0" + currentHours.getSeconds()) : String(currentHours.getSeconds());
 
-    var dia = String(horasAtual.getDate()).length == 1 ? String("0" + horasAtual.getDate()) : String(horasAtual.getDate());
-    var ano = String(horasAtual.getFullYear());
+    var day = String(currentHours.getDate()).length == 1 ? String("0" + currentHours.getDate()) : String(currentHours.getDate());
+    var year = String(currentHours.getFullYear());
 
-    var mesAtual = horasAtual.getMonth();
-    var mes = new Array(12);
-    mes[0] = 'Janeiro';
-    mes[1] = 'Fevereiro';
-    mes[2] = 'Março';
-    mes[3] = 'Abril';
-    mes[4] = 'Maio';
-    mes[5] = 'Junho';
-    mes[6] = 'Julho';
-    mes[7] = 'Agosto';
-    mes[8] = 'Setembro';
-    mes[9] = 'Outubro';
-    mes[10] = 'Novembro';
-    mes[11] = 'Dezembro';
-    mesAtual = (mes[mesAtual]);
+    var currentMonth = currentHours.getMonth();
+    var month = new Array(12);
+    month[0] = 'Janeiro';
+    month[1] = 'Fevereiro';
+    month[2] = 'Março';
+    month[3] = 'Abril';
+    month[4] = 'Maio';
+    month[5] = 'Junho';
+    month[6] = 'Julho';
+    month[7] = 'Agosto';
+    month[8] = 'Setembro';
+    month[9] = 'Outubro';
+    month[10] = 'Novembro';
+    month[11] = 'Dezembro';
+    currentMonth = (month[currentMonth]);
 
-    var diaSemana = horasAtual.getDay();
-    var semana = new Array(6);
-    semana[0] = 'Domingo';
-    semana[1] = 'Segunda-Feira';
-    semana[2] = 'Terça-Feira';
-    semana[3] = 'Quarta-Feira';
-    semana[4] = 'Quinta-Feira';
-    semana[5] = 'Sexta-Feira';
-    semana[6] = 'Sábado';
-    diaSemana = (semana[diaSemana]);
+    var dayWeek = currentHours.getDay();
+    var week = new Array(6);
+    week[0] = 'Domingo';
+    week[1] = 'Segunda-Feira';
+    week[2] = 'Terça-Feira';
+    week[3] = 'Quarta-Feira';
+    week[4] = 'Quinta-Feira';
+    week[5] = 'Sexta-Feira';
+    week[6] = 'Sábado';
+    dayWeek = (week[dayWeek]);
 
-    document.getElementById('h').textContent = horas;
-    document.getElementById('m').textContent = minutos;
-    document.getElementById('s').textContent = segundos;
+    document.getElementById('h').textContent = hours;
+    document.getElementById('m').textContent = minutes;
+    document.getElementById('s').textContent = seconds;
 
-    document.getElementById('divData').textContent = diaSemana + ", " + dia + " de " + mesAtual + " de " + ano;
+    document.getElementById('divDate').textContent = dayWeek + ", " + day + " de " + currentMonth + " de " + year;
 }
 
-// Método do Calendário
+let calendar = document.querySelector('.calendar')
 
-let calendario = document.querySelector('.calendario')
+const nomonthmonthes = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro ', 'Dezembro']
 
-const nomesMeses = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro ', 'Dezembro']
-
-bissexto = (ano) => {
-    return (ano % 4 === 0 && ano % 100 !== 0 && ano % 400 !== 0) || (ano % 100 === 0 && ano % 400 === 0)
+leap = (year) => {
+    return (year % 4 === 0 && year % 100 !== 0 && year % 400 !== 0) || (year % 100 === 0 && year % 400 === 0)
 }
 
-diasFevereiro = (ano) => {
-    return bissexto(ano) ? 29 : 28
+daysFebruary = (year) => {
+    return leap(year) ? 29 : 28
 }
 
-gerar = (mes, ano) => {
-    let diasCalendario = calendario.querySelector('.diasCalendario')
-    let cabecalhoCalendario = calendario.querySelector('#ano')
-    let diasMes = [31, diasFevereiro(ano), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-    diasCalendario.innerHTML = ''
+generate = (month, year) => {
+    let calendarDays = calendar.querySelector('.calendarDays')
+    let cabecalhocalendar = calendar.querySelector('#year')
+    let daysmonth = [31, daysFebruary(year), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    calendarDays.innerHTML = ''
 
     let data = new Date()
-    if (!ano) ano = data.getFullYear()
+    if (!year) year = data.getFullYear()
 
-    let m = `${nomesMeses[mes]}`
-    mesSelecionado.innerHTML = m
-    cabecalhoCalendario.innerHTML = ano
+    let m = `${nomonthmonthes[month]}`
+    selectedMonth.innerHTML = m
+    cabecalhocalendar.innerHTML = year
 
-    let primeiro = new Date(ano, mes, 1)
+    let first = new Date(year, month, 1)
 
-    for (let i = 0; i <= diasMes[mes] + primeiro.getDay() - 1; i++) {
-        let dia = document.createElement('div')
-        if (i >= primeiro.getDay()) {
-            dia.classList.add('calendar-day-hover')
-            dia.innerHTML = i - primeiro.getDay() + 1
-            dia.innerHTML += `<span></span>
+    for (let i = 0; i <= daysmonth[month] + first.getDay() - 1; i++) {
+        let day = document.createElement('div')
+        if (i >= first.getDay()) {
+            day.classList.add('calendar-day-hover')
+            day.innerHTML = i - first.getDay() + 1
+            day.innerHTML += `<span></span>
                             <span></span>
                             <span></span>
                             <span></span>`
-            if (i - primeiro.getDay() + 1 === data.getDate() && ano === data.getFullYear() && mes === data.getMonth()) {
-                dia.classList.add('curr-date')
+            if (i - first.getDay() + 1 === data.getDate() && year === data.getFullYear() && month === data.getMonth()) {
+                day.classList.add('curr-date')
             }
         }
-        diasCalendario.appendChild(dia)
+        calendarDays.appendChild(day)
+
     }
 }
 
-let listaMes = calendario.querySelector('.listaMes')
+let monthList = calendar.querySelector('.monthList')
 
-nomesMeses.forEach((e, index) => {
-    let mes = document.createElement('div')
-    mes.innerHTML = `<div data-month="${index}">${e}</div>`
-    mes.querySelector('div').onclick = () => {
-        listaMes.classList.remove('show')
+nomonthmonthes.forEach((e, index) => {
+    let month = document.createElement('div')
+    month.innerHTML = `<div data-month="${index}">${e}</div>`
+    month.querySelector('div').onclick = () => {
+        monthList.classList.remove('show')
         m.value = index
-        gerar(index, cy.value)
+        generate(index, cy.value)
     }
-    listaMes.appendChild(mes)
+    monthList.appendChild(month)
 })
 
-let mesSelecionado = calendario.querySelector('#mesSelecionado')
+let selectedMonth = calendar.querySelector('#selectedMonth')
 
-mesSelecionado.onclick = () => {
-    listaMes.classList.add('show')
+selectedMonth.onclick = () => {
+    monthList.classList.add('show')
 }
 
 let data = new Date()
 
-let m = { value: data.getMonth() }
-let cy = { value: data.getFullYear() }
+let m = {
+    value: data.getMonth()
+}
+let cy = {
+    value: data.getFullYear()
+}
 
-gerar(m.value, cy.value)
+generate(m.value, cy.value)
 
-document.querySelector('#anoAnterior').onclick = () => {
+document.querySelector('#lastYear').onclick = () => {
     --cy.value
-    gerar(m.value, cy.value)
+    generate(m.value, cy.value)
 }
 
-document.querySelector('#proximoAno').onclick = () => {
+document.querySelector('#nextYear').onclick = () => {
     ++cy.value
-    gerar(m.value, cy.value)
+    generate(m.value, cy.value)
 }
 
-window.addEventListener('load', atualizarHoras);
-setInterval(atualizarHoras, 1000);
+window.addEventListener('load', updateHours);
+setInterval(updateHours, 1000);
